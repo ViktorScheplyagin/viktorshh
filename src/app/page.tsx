fdff26884c79bae875659c6c48706faa9148dbee
Viktor Schepliahin <silverman995@gmail.com>
2023-11-09 17:06:58 +0100

    root page: fetch the data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 import { TerminalOutputAnimation } from "@/components";
+import { IBiography } from "./api/bio/interfaces";
 
-const Home = () => (
-  <TerminalOutputAnimation duration={0.5}>
-    I&apos;m a seasoned web front-end developer with a passion for crafting
-    remarkable user experiences. My journey began with React and TypeScript, but
-    I&apos;ve also explored back-end technologies like NestJS, Docker, MongoDB,
-    and MySQL. My focus is on becoming a full-stack web developer, specializing
-    in Angular and NestJS. Clean, scalable code using object-oriented
-    programming and SOLID principles is my mantra. I&apos;m always learning and
-    seeking the best practices to enhance my skills. Explore my projects and get
-    in touch to discuss web development.
-  </TerminalOutputAnimation>
-);
+const Home = async () => {
+  const result: Response = await fetch("http://localhost:3000/api/bio", {
+    cache: "no-cache",
+  });
+  const data: IBiography = await result.json();
+
+  return (
+    <>
+      <TerminalOutputAnimation duration={0.5}>
+        {data.content}
+      </TerminalOutputAnimation>
+    </>
+  );
+};
 
 export default Home;
