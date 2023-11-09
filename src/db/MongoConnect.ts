dc01c6de5eceefe44603bad83d1dd1b08ba05d72
Viktor Schepliahin <silverman995@gmail.com>
2023-11-09 16:42:15 +0100

    add basic mongodb connections control, using the mongoose client

---
+++ src/db/MongoConnect.ts
@@ -0,0 +1,14 @@
+// db/connect.ts
+import mongoose from "mongoose";
+
+export async function connectMongoDB() {
+  if (!mongoose.connection.readyState) {
+    await mongoose.connect("mongodb://localhost:27017/cv2");
+  }
+}
+
+export async function closeMongoDB() {
+  if (mongoose.connection.readyState) {
+    await mongoose.connection.close();
+  }
+}
