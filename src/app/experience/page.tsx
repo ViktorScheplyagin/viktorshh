1d4f8177905d277c1d77933398bef06402dc35a2
Viktor Schepliahin <silverman995@gmail.com>
2023-11-09 17:04:00 +0100

    experience page: fetch the data

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,13 +1,21 @@
 import { TerminalOutputAnimation } from "@/components";
+import { IPosition } from "@/app/api/positions/interfaces";
 import { Position } from "./_components";
-import { KikiDevelopment, ITransition, Zone3000 } from "./data";
 
-const Experience = () => (
-  <TerminalOutputAnimation duration={2}>
-    <Position data={Zone3000} />
-    <Position data={ITransition} />
-    <Position data={KikiDevelopment} />
-  </TerminalOutputAnimation>
-);
+const Experience = async () => {
+  const response: Response = await fetch(
+    "http://localhost:3000/api/positions",
+    { cache: "no-cache" }
+  );
+  const positions: IPosition[] = await response.json();
+
+  return (
+    <TerminalOutputAnimation duration={2}>
+      {positions.map((position, index) => (
+        <Position key={index} data={position} />
+      ))}
+    </TerminalOutputAnimation>
+  );
+};
 
 export default Experience;
