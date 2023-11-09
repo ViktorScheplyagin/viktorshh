0a9c117bdd77ef96f3ea2894fde87f7eec7e6007
Viktor Schepliahin <silverman995@gmail.com>
2023-11-09 16:53:52 +0100

    education page: fetch the data; update the template

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -1,14 +1,29 @@
 import { TerminalOutputAnimation } from "@/components";
+import { IEducationData } from "@/app/api/education/interfaces";
 
-const Education = () => (
-  <TerminalOutputAnimation duration={0.5}>
-    <h2 className="text-xl">Bachelor&apos;s Degree in System Engineering</h2>
-    <h3 className="text-lg leading-relaxed">
-      Odesa National Polytechnic University
-    </h3>
-    <h3 className="text-lg leading-relaxed">Location: Odesa, Ukraine</h3>
-    <h3 className="text-lg leading-relaxed">Year of Graduation: 2019</h3>
-  </TerminalOutputAnimation>
-);
+const Education = async () => {
+  const response = await fetch("http://localhost:3000/api/education", {
+    cache: "no-cache",
+  });
+  const educationData: IEducationData = await response.json();
+
+  // TODO: use grid
+  return (
+    <TerminalOutputAnimation duration={0.5}>
+      <div className="text-xl">Degree: {educationData.degree}</div>
+      <div>Branch: {educationData.branch}</div>
+      <div className="text-lg leading-relaxed">
+        University: {educationData.university}
+      </div>
+      <div className="text-lg leading-relaxed">
+        Location: {educationData.location}
+      </div>
+      <div className="text-lg leading-relaxed">
+        Education years: {educationData.enrollmentYear} -{" "}
+        {educationData.graduationYear}
+      </div>
+    </TerminalOutputAnimation>
+  );
+};
 
 export default Education;
