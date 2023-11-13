c6b54a78d34037d804ed7f53774dd51c8e3a752b
Viktor Schepliahin <silverman995@gmail.com>
2023-11-13 18:36:19 +0100

    keep the api routes  in an enum

---
+++ src/constants/api.ts
@@ -0,0 +1,8 @@
+export const API_ROOT = "http://localhost:3000/api";
+
+export enum API_ROUTES {
+  BIO = `${API_ROOT}/bio`,
+  EDUCATION = `${API_ROOT}/education`,
+  POSITIONS = `${API_ROOT}/positions`,
+  SKILLS = `${API_ROOT}/skills`,
+}
